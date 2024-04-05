7ec572de2159f17cf95dac504d35e0f4b1db1511
Smartysonu <sonalpriya655@gmail.com>
2024-04-05 11:27:27 +0530

    .env removed

--- controllers/product.js
+++ controllers/product.js
@@ -9,18 +9,18 @@ const Product = require("../models/product")
 //     res.status(200).json({msg: "I am getAllProductsTesting"});
 // };
 
-//filter sort select functionality
+// sort && select functionality
 const getAllProducts = async(req, res) => {
     const { company, name, feature, sort, select } = req.query;
     const queryObject = {};
     let apiData = Product.find(queryObject);
 
     if (company){
-        queryObject.company =company;
+        queryObject.company = company;
     }
     
     if (feature){
-        queryObject.feature =feature;
+        queryObject.feature = feature;
     }
     
     if (name){
@@ -37,10 +37,10 @@ const getAllProducts = async(req, res) => {
         apiData = apiData.select(selectFix);
     }
 
-//pagination Implementation 
+// pagination Implementation 
     let page = Number(req.query.page) || 1;
-    let limit = Number(req.query.limit) ;
-    let skip = (page -1) * limit;
+    let limit = Number(req.query.limit) || 3;
+    let skip = (page - 1) * limit;
 
     apiData = apiData.skip(skip).limit(limit);
 
@@ -48,7 +48,7 @@ const getAllProducts = async(req, res) => {
     res.status(200).json({myData, nbHits: myData.length}); 
 };
 
-//search by query params
+//search && filter by query params
 
 const getAllProductsTesting = async(req, res) => {
     const myData = await Product.find(req.query)
